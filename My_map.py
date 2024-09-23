aab2df322752117609df8f86abc1a8e0cfed70c3
ArturoOliGal <arturoolivaresgallegos@gmail.com>
2024-09-23 15:50:36 -0600

    prueba button

--- My_map.py
+++ My_map.py
@@ -32,15 +32,18 @@ def mapa_pedregal():
 
 casas_Ped=mapa_pedregal()
 df = generadore_clientes()
+weights=[1,1,1]
+col1, col2, col3=st.columns(weights)
 
-st.markdown("""
-    <a href="https://app.powerbi.com/groups/4c07734f-f271-4be1-903b-cfa6cb10c07c/reports/2eb52716-7702-4c09-aefd-c6c2fe07e3ed/3757ea1fa3d08184590d?experience=power-bi" target="_blank">
-        <button style="background-color: #4CAF50; border: none; color: white; padding: 10px 20px;
-        text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 5px; align: center">
-            Ir al powerBI
-        </button>
-    </a>
-    """, unsafe_allow_html=True)
+with col2:
+    st.markdown("""
+        <a href="https://app.powerbi.com/groups/4c07734f-f271-4be1-903b-cfa6cb10c07c/reports/2eb52716-7702-4c09-aefd-c6c2fe07e3ed/3757ea1fa3d08184590d?experience=power-bi" target="_blank">
+            <button style="background-color: #4CAF50; border: none; color: white; padding: 10px 20px;
+            text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 5px;">
+                Ir al powerBI
+            </button>
+        </a>
+        """, unsafe_allow_html=True)
 
 filtro_ubi=st.sidebar.multiselect('Filtro',df['Ubicacion'].unique())
 
