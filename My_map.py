9fdb5e705c17402a92c572f7287e4d30ea8d1b4e
ArturoOliGal <arturoolivaresgallegos@gmail.com>
2024-09-23 15:41:57 -0600

    prueba button

--- My_map.py
+++ My_map.py
@@ -34,7 +34,7 @@ casas_Ped=mapa_pedregal()
 df = generadore_clientes()
 
 st.markdown("""
-    <a href="https://www.youtube.com" target="_blank">
+    <a href="https://app.powerbi.com/groups/4c07734f-f271-4be1-903b-cfa6cb10c07c/reports/2eb52716-7702-4c09-aefd-c6c2fe07e3ed/3757ea1fa3d08184590d?experience=power-bi" target="_blank">
         <button style="background-color: #4CAF50; border: none; color: white; padding: 10px 20px;
         text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 5px;">
             Ir al powerBI
