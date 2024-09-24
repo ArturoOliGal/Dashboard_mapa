2c1d9b23c8db8acbed50b382274e11134c81c562
ArturoOliGal <arturoolivaresgallegos@gmail.com>
2024-09-24 12:13:50 -0600

    prueba tamaño

--- My_map.py
+++ My_map.py
@@ -231,7 +231,7 @@ with col2:
 
 st.markdown("""
     <a href="#top">
-        <button style="position: fixed; bottom: 10px; right: 10px; background-color: #4CAF50; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;">
+        <button style="position: fixed; bottom: 10px; center: 10px; background-color: #4CAF50; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;">
             Ir Arriba
         </button>
     </a>
