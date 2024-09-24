76fb637212c061b7b6555bd8a97e4a3df741178e
ArturoOliGal <arturoolivaresgallegos@gmail.com>
2024-09-24 12:24:56 -0600

    prueba tamaño

--- My_map.py
+++ My_map.py
@@ -232,8 +232,8 @@ with col2:
 st.markdown("""
     <footer style ="text-align: center">
         <a href="#top">
-            <button style="position: fixed; bottom: 10px; center: 10px; background-color: #4CAF50; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;">
-                <i class="bi bi-arrow-up-circle"></i>
+            <button class="bi bi-arrow-up-circle"; style="position: fixed; bottom: 10px; center: 10px; background-color: #4CAF50; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;">
+                
             </button>
         </a>
     </footer>    
