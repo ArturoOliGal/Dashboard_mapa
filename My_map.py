9f9962d90c307c69c722b6e5142dc6e52ceb5a34
José Arturo Olivares Gallegos <arturoolivaresgallegos@gmail.com>
2024-09-24 15:24:49 -0600

    prueba

--- My_map.py
+++ My_map.py
@@ -10,9 +10,9 @@ from folium.plugins import MarkerCluster
 from folium.features import CustomIcon
 from pathlib import Path
 
-st.markdown('<div id="top"></div>', unsafe_allow_html=True)
 icon_path = Path("Imagenes/Isotipo_Super.png")
 st.set_page_config(page_title="DENUE dashboard", page_icon=str(icon_path),layout='wide')
+st.markdown('<div id="top"></div>', unsafe_allow_html=True)
 
 ruta_imagen = "Imagenes/ubicacion.png"  
 ruta_mapa="Imagenes/Imagen Pedregal.png"
