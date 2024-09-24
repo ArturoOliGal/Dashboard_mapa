1b213968d51435ed9bad5615e01030d316f51360
ArturoOliGal <arturoolivaresgallegos@gmail.com>
2024-09-24 12:24:04 -0600

    prueba tamaño

--- My_map.py
+++ My_map.py
@@ -233,7 +233,7 @@ st.markdown("""
     <footer style ="text-align: center">
         <a href="#top">
             <button style="position: fixed; bottom: 10px; center: 10px; background-color: #4CAF50; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; font-size: 16px; border-radius: 5px;">
-                Ir Arriba
+                <i class="bi bi-arrow-up-circle"></i>
             </button>
         </a>
     </footer>    
